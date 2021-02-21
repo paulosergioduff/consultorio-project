17269cea829f84313482edc81bcebdcb01bebbbb
paulosergioduff <paulosergioduff@gmail.com>
2021-02-21 18:07:05 +0000

    Plugin clipboard

--- testeapk/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ testeapk/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-clipboard.Clipboard",
+    "file": "plugins/cordova-clipboard/www/clipboard.js",
+    "pluginId": "cordova-clipboard",
+    "clobbers": [
+      "cordova.plugins.clipboard"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.4"
+  "cordova-plugin-whitelist": "1.3.4",
+  "cordova-clipboard": "1.3.0"
 };
 // BOTTOM OF METADATA
 });
